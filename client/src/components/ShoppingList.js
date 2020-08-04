9510a00c313b3b44cb5ec94b2b872be152c50f46
Michal <michal.rojek.94@gmail.com>
2020-08-04 22:25:58 +0200

    Added simple form for new product and way of submiting it to backend

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -5,14 +5,57 @@ import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 
 
-//TODO: divide components to container and presentional components i.e. list component, modal component
+//TODO: divide components to container and presentional components i.e. list component, modal component, form component (inputs in props as in state here)
+//TODO: prepare form for adding new product
+//TODO: add function for posting form data to backend endpoint
+//TODO: prepare list refreshing after adding new item
+//TODO: check if this form of <Modal><Form><Modal> is really okay (seems to work)
+//TODO: add some sort of validation to form, add message prompt after succesful add
+//TODO: clear values in state after closing modal
 class ShoppingList extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             items: [],
-            isModalOpen: false
+            isModalOpen: false,
+            inputs: {
+                name: {
+                    type: 'text',
+                    value: ''
+                },
+                //TODO: change description to textarea
+                description: {
+                    type: 'text',
+                    value: ''
+                },
+                //TODO: is step='any' okay for all umber fields?
+                weight: {
+                    type: 'number',
+                    value: 0
+                },
+                //TODO change unit to combobox and put it in same line as weight
+                unit: {
+                    type: 'text',
+                    value: ''
+                },
+                calories: {
+                    type: 'number',
+                    value: 0
+                },
+                protein: {
+                    type: 'number',
+                    value: 0
+                },
+                carbohydrates: {
+                    type: 'number',
+                    value: 0
+                },
+                fat: {
+                    type: 'number',
+                    value: 0
+                }
+            }
         }
     }
 
@@ -31,13 +74,35 @@ class ShoppingList extends Component {
         this.setState({isModalOpen: !this.state.isModalOpen});
     }
 
-    submitData = (event) => {
-        event.preventDefault();
-        console.log(event.target.exampleEmail.value)
+    onChange = (e) => {
+        e.preventDefault();
+        const { value, id, type } = e.target;
+        console.log(e.target)
+        this.setState(({inputs}) => ({
+            inputs: {...inputs, [id]: {...inputs[id], value: type === "number" ? parseFloat(value) : value}}
+        }));
+    }
+
+    submitData = (e) => {
+        e.preventDefault();
+        const {inputs} = this.state
+        const productEntries = Object.entries(inputs).map(([key, {value}]) => {
+            return [key, value];
+        });
+        const product = Object.fromEntries(productEntries);
+        axios.post('http://localhost:5000/api/products', product).then((response) => {
+            console.log(response)
+            this.toggle();
+            window.location.reload(false); 
+        })
+    }
+
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
     render() {
-        const { items } = this.state;
+        const { items, inputs } = this.state;
         return (
             <Container className="diet-tracker-list">
                 <Button
@@ -49,29 +114,12 @@ class ShoppingList extends Component {
                     <Form onSubmit={this.submitData}>
                         <ModalHeader toggle={this.toggle}>Add new product</ModalHeader>
                         <ModalBody>
+                            {Object.entries(inputs).map(([key, {type, value}]) => (
                                 <FormGroup>
-                                    <Label for="exampleEmail">Email</Label>
-                                    <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" onChange={this.onChange}/>
+                                    <Label for={key}>{this.capitalizeFirstLetter(key)}</Label>
+                                    <Input type={type} step="any" name={key} id={key} placeholder={this.capitalizeFirstLetter(key)} value={value} onChange={this.onChange}/>
                                 </FormGroup>
-                                <FormGroup>
-                                    <Label for="username">Username</Label>
-                                    <Input name="username" id="username" placeholder="username" onChange={this.onChange}/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="examplePassword">Password</Label>
-                                    <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" onChange={this.onChange}/>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="confirmPassword">Confirm password</Label>
-                                    <Input 
-                                        invalid={this.state.invalidPassword}
-                                        type="password"
-                                        name="confirmPassword"
-                                        id="confirmPassword"
-                                        placeholder="confirm password"
-                                        onChange={this.onChange}
-                                    />
-                                </FormGroup>
+                            ))}
                         </ModalBody>
                         <ModalFooter>
                             <Button color="primary" type="submit">Submit</Button>{' '}
